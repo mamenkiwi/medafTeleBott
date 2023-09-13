4cb845604f2de1ec246259f164262adf27cc35d9
mamenkiwi <mamenkiwi@gmail.com>
2023-09-13 16:03:20 +0000

    vfd

---
+++ index.js
@@ -0,0 +1,27 @@
+import { Telegraf } from "telegraf";
+//import { Bot } from "grammy";
+import express from 'express'
+const token = "6395374268:AAEK2E4sbVWf3HLaIhTYWDnvQiKrQkb5GAw"
+const bot = new Telegraf(token);
+const web_link = "https://alphaspecialschool.vercel.app/";
+let sandBoxLink ='https://6cllf7.csb.app/'
+const app = express()
+
+app.get('/', (req, res)=> {
+    res.send('working')
+})
+app.use(await bot.createWebhook({domain: sandBoxLink }))
+bot.start((ctx) =>
+  ctx.reply("Welcome :)))))", {
+    
+    reply_markup: {
+    inline_keyboard  : [[{ text: "Place your Bid", web_app: { url: sandBoxLink },  }]],
+        resize_keyboard: true,
+      
+    },
+  })
+);
+
+bot.launch();
+
+app.listen(8080, ()=> console.log('app running on port 8080'))
