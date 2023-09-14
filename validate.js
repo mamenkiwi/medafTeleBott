68970893e707a5662422e24aa835065d2d0e9c7b
mamenkiwi <mamenkiwi@gmail.com>
2023-09-14 13:51:44 +0000

    sdged

---
+++ validate.js
@@ -0,0 +1,22 @@
+import CryptoJS from 'crypto-js'
+
+
+let TELEGRAM_BOT_TOKEN='6395374268:AAEK2E4sbVWf3HLaIhTYWDnvQiKrQkb5GAw'
+
+export async function verifyTelegramWebAppData(telegramInitData){
+    return new Promise((resolve)=>{
+let initData = new URLSearchParams(telegramInitData)
+const hash = initData.get("hash");
+  let dataToCheck= [];
+
+  initData.sort();
+  initData.forEach((val, key) => key !== "hash" && dataToCheck.push(`${key}=${val}`));
+  
+  const secret = CryptoJS.HmacSHA256(TELEGRAM_BOT_TOKEN, "WebAppData");
+  const _hash = CryptoJS.HmacSHA256(dataToCheck.join("\n"), secret).toString(CryptoJS.enc.Hex);
+  
+resolve(_hash === hash)
+
+
+    })
+}
