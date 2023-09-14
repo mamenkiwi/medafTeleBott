eba8768c3df226aa94cd3f230e85983db6e7ac25
mamenkiwi <mamenkiwi@gmail.com>
2023-09-14 15:15:13 +0000

    csdf

--- index.js
+++ index.js
@@ -2,21 +2,40 @@ import { Telegraf } from "telegraf";
 //import { Bot } from "grammy";
 import express from 'express'
 import { verifyTelegramWebAppData } from "./validate.js";
+import morgan from "morgan"
+
+
+//'http://api.telegram.org/bot6395374268:AAEK2E4sbVWf3HLaIhTYWDnvQiKrQkb5GAw/setWebhook?url=https://medaftelebott-3-f0578401.deta.app/'
+const app = express()
 const token = "6395374268:AAEK2E4sbVWf3HLaIhTYWDnvQiKrQkb5GAw"
 const bot = new Telegraf(token);
 const web_link = "https://alphaspecialschool.vercel.app/";
 let sandBoxLink ='https://6cllf7.csb.app/'
-let webhookLink ='https://medaftelebott-3-f0578401.deta.app/'
+let webhookLink ='https://medaftelebott-4-f0578401.deta.app'
 
-'http://api.telegram.org/bot6395374268:AAEK2E4sbVWf3HLaIhTYWDnvQiKrQkb5GAw/setWebhook?url=https://medaftelebott-3-f0578401.deta.app/'
-const app = express()
+app.use(morgan('dev'))
 
-app.use(express.json())
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use(await bot.createWebhook({domain: webhookLink }))
 
 app.post('/checkData', async (req,res)=>{
   let {data} = req.body
-let valRes = await verifyTelegramWebAppData(data._auth)
+  console.log(data)
+let valRes = await verifyTelegramWebAppData(data._auth, token)
 console.log(valRes)
 
 res.send(valRes)
--- validate.js
+++ validate.js
@@ -1,9 +1,9 @@
 import CryptoJS from 'crypto-js'
 
 
-let TELEGRAM_BOT_TOKEN='6395374268:AAEK2E4sbVWf3HLaIhTYWDnvQiKrQkb5GAw'
+//let TELEGRAM_BOT_TOKEN='6395374268:AAEK2E4sbVWf3HLaIhTYWDnvQiKrQkb5GAw'
 
-export async function verifyTelegramWebAppData(telegramInitData){
+export async function verifyTelegramWebAppData(telegramInitData, token){
     return new Promise((resolve)=>{
 let initData = new URLSearchParams(telegramInitData)
 const hash = initData.get("hash");
@@ -12,7 +12,7 @@ const hash = initData.get("hash");
   initData.sort();
   initData.forEach((val, key) => key !== "hash" && dataToCheck.push(`${key}=${val}`));
   
-  const secret = CryptoJS.HmacSHA256(TELEGRAM_BOT_TOKEN, "WebAppData");
+  const secret = CryptoJS.HmacSHA256(token, "WebAppData");
   const _hash = CryptoJS.HmacSHA256(dataToCheck.join("\n"), secret).toString(CryptoJS.enc.Hex);
   
 resolve(_hash === hash)
